f6194b541a34e9ea442c7bd471b7d09822f55fc6
Foows - (Dave) <dbangcoyo11@gmail.com>
2026-04-07 23:40:22 +0800

    Update about dialog UI

---
+++ script.js
@@ -0,0 +1,4 @@
+const dialog_photo = document.getElementById('dialog_photo');
+const myDialog = document.getElementById('myDialog');
+
+dialog_photo.addEventListener('click', () => myDialog.showModal());
